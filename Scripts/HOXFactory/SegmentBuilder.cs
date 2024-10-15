e3365be68c3940a8fedc4248850b8a5eed530a5e
Jared Blackburn <BlackJar72@yahoo.com>
2024-10-15 11:51:13 -0400

    Basic building is working; now need to generate the plan

---
+++ Scripts/HOXFactory/SegmentBuilder.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+namespace kfutils.hox.factory {
+
+    /// This is to be for generating segments.
+    /// Should this be an actual class?  Or an interface for creating standardized, use specific classes.
+
+    [System.Serializable]
+    public class SegmentBuilder {
+
+    }
+
+
+}
---
+++ Scripts/HOXFactory/SegmentFactory.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+
+namespace kfutils.hox.factory {
+
+
+    [System.Serializable]
+    public class SegmentFactory {
+        public GameObject prefab;
+        public bool typicallyMirrorRotations = true;
+
+    }
+
+
+}
